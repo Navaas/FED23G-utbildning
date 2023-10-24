59cc364d673fc47968b08ffbc028c159e04e94c3
Michaela <mi-and@outlook.com>
2023-10-24 14:33:49 +0200

    Javascript intro del 2

---
+++ JavaScript intro/Intro 2/script.js
@@ -0,0 +1,36 @@
+function clickedButton() {
+  const text = prompt("Skriv något här..");
+  const text1 = prompt("Skriv något här..");
+  const text2 = prompt("Skriv något här..");
+  const text3 = prompt("Skriv något här..");
+  const text4 = prompt("Skriv något här..");
+  console.log(text, text1, text2, text3, text4);
+}
+
+clickedButton();
+
+function calculate() {
+  const num1 = Number(prompt("Välj ett nummer"));
+  const operator = prompt("Välj + - * /");
+  const num2 = Number(prompt("Välj ett nummer"));
+  console.log(num1, operator, num2);
+
+  let result; // Här används let för att det är olika resultat
+
+  // I denna if else så kollar den vilken operator + - * / som används som användaren har använt.
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
+  } else {
+    console.log("Skriv in en korrekt operator");
+  }
+  if (result) {
+    alert(result); // Resultatet kommer ut i en alert ruta
+    console.log(result);
+  }
+}
