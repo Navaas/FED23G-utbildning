d9839ef046629dd863a833e6676d6be19658ff52
Michaela <mi-and@outlook.com>
2023-10-26 12:37:01 +0200

    start up

---
+++ JavaScript intro/Hamburgemeny/README.md
@@ -0,0 +1,13 @@
+# Hamburgemenu
+
+### Metoder:
+
+---
+
+**Den kommer byggas med:**
+
+- HTML5
+- CSS
+- JavaScript
+
+- Kommentarer i koden (Bra anteckningar för mig)
---
+++ JavaScript intro/Hamburgemeny/script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menuIcon");
+  if (menu.classList.toggle === "block") {
+    menu.classList.toggle === "none";
+    console.log(menu);
+  } else {
+    menu.classList.toggle === "block";
+  }
+}
+toggleMenu();
