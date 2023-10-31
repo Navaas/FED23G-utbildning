f08e9ede3f2b1001aff349af9b1da9ebefcdfce7
Michaela <mi-and@outlook.com>
2023-10-31 12:11:10 +0100

    add javascript

--- JavaScript intro/Inlamning/script.js
+++ JavaScript intro/Inlamning/script.js
@@ -51,6 +51,8 @@ function search() {
   const takeStuff = document.getElementById("takeStuffFrom").value;
   const saveFromTakeStuff = takeStuff;
   takeStuff.innerText = saveFromTakeStuff;
+  const pickedStuff = ["Test"];
+
   const stuffs = ["Tvål", "Tandborste", "Schampo", "Balsam", "Hudlotion"];
   //const make = prompt("Hämta en sak");
   const makeIndex = stuffs.indexOf(takeStuff);
@@ -60,11 +62,54 @@ function search() {
   for (const stuff of stuffs) {
     if (stuff === takeStuff) {
       console.log(stuff + " finns i ditt badrumsskåp");
-      stuffs.splice(makeIndex, 1); // splice gör att den tar bprt objekt i arrayen
+      stuffs.splice(makeIndex, 1);
+      pickedStuff.push(stuff);
+      console.log(pickedStuff);
+      h4.innerText = takeStuff + "Finns i skåpet"; // splice gör att dten tar bort objekt i arrayen
+    } else {
     }
   }
   console.log(takeStuff + " finns inte i ditt badrumsskåp");
   console.log(stuffs);
-  h4.innerText = stuffs;
+  h4.innerText = takeStuff + "Finns inte skåpet";
+
   document.body.appendChild(h4);
+}
+
+// Här ligger script för köket (kitchen.html)
+
+const foodModal = document.getElementById("foodModal");
+const btnModal = document.getElementById("btnModal");
+const span = document.getElementsByClassName("close"[0]);
+
+function clickOnBtn() {
+  foodModal.style.display = "block";
+}
+
+function clickOnSpan() {
+  foodModal.style.display = "none";
+}
+
+// Funkar inte, följt w3 schools
+/*function clickOutsideModal(event) {
+  if (event.target == foodModal) {
+    foodModal.style.display = "none";
+  }
+}
+clickOutsideModal();
+*/
+
+// Här är scriptet för matlistan
+
+function foodList() {
+  const food = ["Gurka", "Sallad", "Mjöl", "Yoghurt", "Smör", "Ost"];
+  const foodInputOne = document.getElementById("foodInputOne").value;
+  const foodInputTwo = document.getElementById("foodInputTwo").value;
+
+  const text1 = document.getElementById("listFood");
+  text1.innerText = foodInputOne + foodInputTwo;
+  food.push(foodInputOne, foodInputTwo);
+
+  console.log(food, foodInputOne, foodInputTwo);
+  console.table(food);
 }
