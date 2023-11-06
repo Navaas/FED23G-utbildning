8b56172f3fc8024179ee207a6d4badb7239fb0fe
Michaela <mi-and@outlook.com>
2023-11-06 10:28:11 +0100

    new project with javascript and data

---
+++ JavaScript intro/localStorageCart/data.js
@@ -0,0 +1,20 @@
+// Detta är en mockup på data. Hitta på data
+// En lista med objekt i en array
+
+const products = [
+  {
+    id: 1,
+    title: "One",
+    price: 45,
+  },
+  {
+    ide: 2,
+    title: "Göteborgs Rapé",
+    price: 50,
+  },
+  {
+    id: 3,
+    title: "General",
+    price: 60,
+  },
+];
