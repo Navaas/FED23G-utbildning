b78f021101c7d3dcf00ea4c399caa1e343c48209
Michaela <mi-and@outlook.com>
2023-11-06 11:14:03 +0100

    show product title on screen

--- JavaScript intro/localStorageCart/main.js
+++ JavaScript intro/localStorageCart/main.js
@@ -6,5 +6,20 @@ function main() {
 }
 
 function renderProducts() {
-  console.log(products); // "Poducts" känner den av för att den ligger i det globala scoopet i html filen.
+  console.log(products); // "Poducts / data.js"  hittas för att den ligger i det globala scoopet i html filen.
+}
+
+function renderProducts() {
+  const main = document.querySelector("#productContainer");
+  // Läs den såhär: För varje produkt i "products" (produktlistan)
+
+  // Foor Loop för att mappa ut listan
+  for (const product of products) {
+    //console.log(product); // Nu syns listan i consolen
+    const card = document.createElement("div"); // Skapar en div
+    card.textContent = product.title; // Vill att title ska visas
+
+    // Var det ska ligga, nu i main. Funkar med getByElementById också
+    main.append(card);
+  }
 }
