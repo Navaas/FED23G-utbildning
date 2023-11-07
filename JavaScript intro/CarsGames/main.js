f25e1d831204073065eb823d65fa413e0f9b5921
Michaela <mi-and@outlook.com>
2023-11-07 12:52:54 +0100

    update

--- JavaScript intro/CarsGames/main.js
+++ JavaScript intro/CarsGames/main.js
@@ -12,6 +12,9 @@ function showScene() {
 
   const buttonArray = document.getElementById("buttonArray");
   buttonArray.textContent = scene.buttonArray;
+  const button = document.querySelector("button-container");
+  const container = document.createElement("div");
+  container.className = "container";
 
   // Hämta data från objecten från scener
   text.textContent = scene.text;
@@ -44,15 +47,16 @@ function showScene() {
     activeSceneIndex = sceneIndex;
     showScene();
   }
+  button.append(buttonArray);
 }
 
-function itemButton() {
-  const items = ["Skiftnyckel", "Däck", "Motorolja", "Bultmaskin", "Papper"];
-  const h2 = document.createElement("h2");
-  for (const item of items) {
-    h2.innerText = item;
+// function itemButton() {
+//   const items = ["Skiftnyckel", "Däck", "Motorolja", "Bultmaskin", "Papper"];
+//   const h2 = document.createElement("h2");
+//   for (const item of items) {
+//     h2.innerText = item;
 
-    console.log(item);
-  }
-  h2.append(items);
-}
+//     console.log(item);
+//   }
+//   h2.append(items);
+// }
