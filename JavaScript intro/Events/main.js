d9ab6f544bc4e0d0483fb541dcd678396ec68a03
Michaela <mi-and@outlook.com>
2023-11-13 20:00:57 +0100

    Style for red circle

--- JavaScript intro/Events/main.js
+++ JavaScript intro/Events/main.js
@@ -1,16 +1,17 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  document.addEventListener("click", placeCircle);
-  document.querySelector("h1").addEventListener("mouseover", setRandomColor);
+  document.addEventListener("click", placeCircle); // Klickar du dyker en röd cirkel upp.
+  document.querySelector("h1").addEventListener("mouseover", setRandomColor); // Hovrar du över byter den färg.
+  document.querySelector("h2").addEventListener("mouseover", setRandomColor); // Hovrar du över byter den färg.
 }
 
+// Detta är funktionen för den röda cirkeln, när man klickar, sen fadar den ut.
 function placeCircle(event) {
-  console.log(event.x, event.y);
   const circle = document.createElement("div");
-  circle.className = "circle";
-  circle.style.left = event.x + "px"; // Man kan skriva clientX
-  circle.style.top = event.y + "px"; // ClientY
+  circle.classList = "circle";
+  circle.style.left = event.clientX + "px";
+  circle.style.top = event.clientY + "px";
 
   document.body.append(circle);
   setTimeout(() => {
@@ -19,13 +20,13 @@ function placeCircle(event) {
 }
 
 function setRandomColor(event) {
-  event.currentTarget.style.color = getRandomColor;
+  event.currentTarget.style.color = getRandomColor();
 }
-
+// Detta är en funktion som väljer färger random
 function getRandomColor() {
-  const chars = "123456789abcdef";
-  const r = chars[Math.floor(Math.floor) * 16];
-  const g = chars[Math.floor(Math.floor) * 16];
-  const b = chars[Math.floor(Math.floor) * 16];
+  const chars = "0123456789abcdef";
+  const r = chars[Math.floor(Math.random() * 16)];
+  const g = chars[Math.floor(Math.random() * 16)];
+  const b = chars[Math.floor(Math.random() * 16)];
   return "#" + r + g + b;
 }
