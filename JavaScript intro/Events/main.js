a8be1b094717990aa0435437a2516a10a3a67294
Michaela <mi-and@outlook.com>
2023-11-13 13:44:15 +0100

    update

---
+++ JavaScript intro/Events/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  document.addEventListener("click", placeCircle);
+}
+
+function placeCircle(event) {
+  console.log(event);
+}
