08bcb7808c66b853bbf4248b14126062e78993f8
Michaela <mi-and@outlook.com>
2023-11-14 08:24:11 +0100

    update code

--- JavaScript intro/youtube-dl/youtube-dl/main.js
+++ JavaScript intro/youtube-dl/youtube-dl/main.js
@@ -2,6 +2,6 @@ const ytdl = require("ytdl-core");
 const fs = require("fs");
 
 const link = "https://www.youtube.com/watch?v=HF0s-pXOHck";
-const filePath = "./newMovie.mov";
+const filePath = "./movie.mov";
 
 ytdl(link, { quality: "highest" }).pipe(fs.createWriteStream(filePath));
