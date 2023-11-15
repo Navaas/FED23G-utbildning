33a444dadff4449f844bfddc56e70e2c0f88acfb
Michaela <mi-and@outlook.com>
2023-11-15 15:03:20 +0100

    two slidshow

--- JavaScript intro/Slidshow/main.js
+++ JavaScript intro/Slidshow/main.js
@@ -1,28 +1,28 @@
 window.addEventListener("DOMContentLoaded", main);
 
-let currentImageIndex = 0;
-
 function main() {
-  beginSlideshow();
+  beginSlideshow("#slideshow1");
+  beginSlideshow("#slideshow2");
 }
 
-function beginSlideshow() {
+function beginSlideshow(selector) {
+  let currentImageIndex = 0;
   setInterval(showNextImage, 2000);
-}
 
-function showNextImage() {
-  // Hämta alla bilder från DOM:en
-  const images = document.querySelectorAll(".slideshowContainer img");
-  // Dölj bilden som är synlig just nu
-  const visibleImage = images[currentImageIndex];
-  visibleImage.classList.remove("show");
-  // Sätt nästa bild index som ska visas
-  currentImageIndex += 1;
-  if (currentImageIndex >= images.length) {
-    currentImageIndex = 0;
+  function showNextImage() {
+    // Hämta alla bilder från DOM:en
+    const images = document.querySelectorAll(selector + " img");
+    // Dölj bilden som är synlig just nu
+    const visibleImage = images[currentImageIndex];
+    visibleImage.classList.remove("show");
+    // Sätt nästa bild index som ska visas
+    currentImageIndex += 1;
+    if (currentImageIndex >= images.length) {
+      currentImageIndex = 0;
+    }
+    // Visa bilden som är dold just nu
+    const hiddenImage = images[currentImageIndex];
+    hiddenImage.classList.add("show");
+    console.log(visibleImage);
   }
-  // Visa bilden som är dold just nu
-  const hiddenImage = images[currentImageIndex];
-  hiddenImage.classList.add("show");
-  console.log(visibleImage);
 }
