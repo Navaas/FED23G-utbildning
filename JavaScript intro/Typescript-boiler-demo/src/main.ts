67404d9d8ec4197d7c38e7185adc5603462758af
Michaela <mi-and@outlook.com>
2023-11-27 13:58:14 +0100

    add functions for button

--- JavaScript intro/Typescript-boiler-demo/src/main.ts
+++ JavaScript intro/Typescript-boiler-demo/src/main.ts
@@ -1,24 +1,29 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+window.addEventListener("DOMContentLoaded", main);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function main() {
+  const button = document.querySelector("button");
+  button?.addEventListener(
+    "click",
+    randomizeBackgroundColor
+  ); /* ? = är en inbyggd if-sats*/
+
+  const fullName = getFullName("My", "Mysansson");
+  console.log(fullName);
+}
+
+/* Denna funktionen sätter en random färg när du trycker på knappen*/
+function randomizeBackgroundColor() {
+  const red = Math.floor(Math.random() * 256); /* Math.floor avrundar neråt*/
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  document.body.style.background = `rgb(${red}, ${green}, ${blue})`;
+}
+
+/* Här kommer den första Typescripten i denna filen*/
+function getFullName(firstName: string, lastName: string) {
+  // return firstName + "" + lastName; /* Denna och raden under gör samma sak*/
+  return `${firstName} ${lastName}`;
+}
