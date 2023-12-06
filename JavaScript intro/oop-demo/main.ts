541c4ef1db5d0579519163fb45b1e3c37cc02dcf
Michaela <mi-and@outlook.com>
2023-12-06 14:40:18 +0100

    create circle, rectangle i oop

---
+++ JavaScript intro/oop-demo/main.ts
@@ -0,0 +1,81 @@
+/* abstract gör att du inte får skapa fler instanser av shape, då får man ärva. Abstract kan bara sättas framför "class" */
+abstract class Shape {
+  protected x: number;
+  protected y: number;
+  protected color: string;
+
+  constructor(x: number, y: number, color: string) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+}
+
+/* --------------------------- Cirklar -------------------------- */
+
+// Shape är förälder för circle. Circle ärver nu från Shape. (protected), "Super ()" " anropar förälderns constructor i detta fall från Shape
+class Circle extends Shape {
+  radius: number;
+
+  constructor(x: number, y: number, color: string, radius: number) {
+    super(x, y, color);
+    this.radius = radius;
+  }
+
+  /* Detta är metoder */
+  calculateArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+
+  calculatePerimeter() {
+    return Math.PI * this.radius * 2;
+  }
+}
+
+const head = new Circle(0, 0, "red", 10);
+const plate = new Circle(100, 100, "white", 12);
+
+/* ----------------------------------- Rektanglar ---------------------------- */
+
+class Rectangle extends Shape {
+  private width: number;
+  private height: number;
+
+  constructor(
+    x: number,
+    y: number,
+    color: string,
+    width: number,
+    height: number
+  ) {
+    super(x, y, color);
+    this.width = width;
+    this.height = height;
+  }
+
+  /* Detta är metoder */
+  public calculateArea() {
+    return this.height * this.width;
+  }
+
+  public calculatePerimeter() {
+    return this.width * 2 + this.height * 2;
+  }
+}
+
+// Skapa nya object
+const rect = new Rectangle(0, 0, "red", 10, 20); // Skapa en ny rektangel (ett nytt object)
+const letter = new Rectangle(100, 100, "white", 12, 12); // Skapa en ny rektangel (ett  nytt object)
+
+const rectArea = rect.calculateArea(); // rect är samma som this
+const letterArea = letter.calculateArea(); // letter är samma som this
+
+// -------------------------consol log cirklar och rektanglar ------------------------ //
+
+console.log("letter", letter.calculateArea(), letter.calculatePerimeter());
+
+console.log("head", head.calculateArea());
+console.log("head", head.calculatePerimeter());
+
+console.log("plate", plate.calculateArea());
+console.log("plate", plate.calculatePerimeter());
