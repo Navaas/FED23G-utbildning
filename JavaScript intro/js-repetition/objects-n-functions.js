7871312ceb18febeb790099379219d46318cf2a9
Michaela <mi-and@outlook.com>
2023-12-18 11:40:23 +0100

    create two orders

---
+++ JavaScript intro/js-repetition/objects-n-functions.js
@@ -0,0 +1,33 @@
+const orders = [
+  {
+    id: 129872,
+    created: "2023-09-12",
+    price: 2999,
+    products: ["Julgran", "Soffa", "Hatt"],
+    customer: "Mysan",
+    deliveryAdress: {
+      street: "Vägen 1",
+      zip: 48733,
+      city: "Gothenburg",
+    },
+  },
+  {
+    id: 287234,
+    created: "2023-12-20",
+    price: 12999,
+    products: ["Dator"],
+    customer: "David",
+    deliveryAdress: {
+      street: "Vägen 1",
+      zip: 48733,
+      city: "Gothenburg",
+    },
+  },
+];
+
+// CRUD = Skapa, Läsa, Uppdatera, Ta bort - Create, Read, Update, Delete
+
+// Create (Write)
+// Read (read)
+// Update (write)
+// Delete (write)
