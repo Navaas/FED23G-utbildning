7ddb5a94237fa855e1b798e12835677b7271a8a3
Michaela <mi-and@outlook.com>
2023-12-20 09:55:18 +0100

    changed from js file to ts file

--- JavaScript intro/js-repetition/objects-n-functions.ts
+++ JavaScript intro/js-repetition/objects-n-functions.ts
@@ -1,4 +1,35 @@
-let orders = [
+// Man kan generera flera olika typer. Mer flexibel. I type förklarar du med typer hur objektet ser ut med string/number/boolean
+type Address = {
+  street: string;
+  zip: number;
+  city: string;
+};
+
+type Order = {
+  id: number;
+  created: string;
+  price: number;
+  products: string[];
+  customer: string;
+  deliveryAdress: Address; // Hämtas från "type Address" så den får den strukturen
+  invoiceAddress?: Address; // "?" är samma som valfri none/undefined. "Address | undefined"
+};
+
+// Omit är detsamma som bortse. Om man vill utesluta vissa saker.
+type OrderCreate = Omit<Order, "id" | "created">;
+
+// "&" innebär att lägga till saker. Här används Order + status och deleted
+// type OrderExtended = Order & {
+//   status: string;
+//   deleted: boolean;
+// }
+
+// Förklarar hur ett object ser ut.
+// interface Order {
+// }
+
+// Lägg till typen "type Order" på variabeln "let orders", så den följer kontraktet/mallen som typen använder.
+let orders: Order[] = [
   {
     id: 12,
     created: "2023-09-12",
@@ -32,7 +63,7 @@ let orders = [
 // Update (write)
 // Delete (write)
 
-function findOrder(id) {
+function findOrder(id: number) {
   for (const order of orders) {
     if (order.id === id) {
       return order;
@@ -42,8 +73,8 @@ function findOrder(id) {
 
 // Predicate är sant eller falskt
 
-function deleteOrder(id) {
-  const newOrdersArray = [];
+function deleteOrder(id: number) {
+  const newOrdersArray: Order[] = [];
   for (const order of orders) {
     if (order.id !== id) {
       newOrdersArray.push(order);
@@ -54,9 +85,9 @@ function deleteOrder(id) {
   // orders = orders.filter((order) => order.id !== id) // Ta bort om id:t matchar, annars behåll ordern.
 }
 
-function createOrder(order) {
+function createOrder(order: OrderCreate) {
   orders.push({
-    id: Math.random().toString().slice(2),
+    id: Number(Math.random().toString().slice(2)),
     created: new Date().toISOString(),
     ...order,
   });
