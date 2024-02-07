16f56020f805064db0b7a514de908b5cc0b60fc8
Michaela <mi-and@outlook.com>
2024-02-07 14:38:41 +0100

    clean it up in app.tsx

--- JavaScript fördjupning/react-state/src/App.tsx
+++ JavaScript fördjupning/react-state/src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+import BucketForm from "./BucketForm";
 import BucketListitem from "./BucketListItem";
 import Header from "./Header";
-import { mockedBucketList } from "./bucketData";
+import { BucketItem, mockedBucketList } from "./bucketData";
 
 function App() {
+  const [bucketList, setBucketList] = useState(mockedBucketList);
+
+  const handleSubmit = (bucketItem: BucketItem) => {
+    setBucketList([
+      bucketItem,
+      /* Spredoperator ta med allt från den befintliga arrayen och lägg till det nya med setBucketList. */
+      ...bucketList,
+    ]);
+    // Hantera sparningen..
+  };
+
   return (
     <>
       <Header /> {/* Mapp och key här ihop. */}
+      <BucketForm onSubmit={handleSubmit} />
       <ul className="p-4 flex flex-col gap-2 text-lg">
-        {mockedBucketList.map((bucketItem) => (
+        {bucketList.map((bucketItem) => (
           <BucketListitem bucketItem={bucketItem} key={bucketItem.id} />
         ))}
       </ul>
