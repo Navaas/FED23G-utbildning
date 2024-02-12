0cdba2319bf8e2b76b730230a55f20af310bb867
Michaela <mi-and@outlook.com>
2024-02-12 13:24:00 +0100

    atart uo new projekt

---
+++ JavaScript fördjupning/react-hooks/src/App.tsx
@@ -0,0 +1,13 @@
+import CountButton from "./CountButton";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <CountButton title="Röd" />
+      <CountButton title="Blå" />
+    </>
+  );
+}
+
+export default App;
---
+++ JavaScript fördjupning/react-hooks/src/CountButton.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+interface Props {
+  title: string;
+}
+
+function CountButton(props: Props) {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button
+        className="px-4 py-2 bg-slate-200 rounded-full"
+        onClick={() => setCount(count + 10)}
+      >
+        {count}
+        {props.title} {/* Hämtar titel "Röd och Blå" från App.tsx */}
+      </button>
+    </>
+  );
+}
+
+export default CountButton;
