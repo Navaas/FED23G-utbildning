8b1a892d979bfc65466e2b63ef071be74eabd193
Michaela <mi-and@outlook.com>
2024-02-14 13:50:55 +0100

    Hämtar api helgdagar och skriver ut det

--- JavaScript fördjupning/fetch-ovning/main.js
+++ JavaScript fördjupning/fetch-ovning/main.js
@@ -1,7 +1,22 @@
 window.addEventListener("DOMContentLoaded", main);
 
-function main() {
-    /* Hämta data */
-const data
+async function main() {
+  const holidays = await getSwedishHolidays();
+  renderHolidays(holidays);
+}
 
+function renderHolidays(holidays) {
+  const ul = document.createElement("ul");
+  for (const holiday of holidays) {
+    const li = document.createElement("li");
+    li.textContent = holiday.helgdag + " - " + holiday.datum;
+    ul.append(li);
+  }
+  document.body.append(ul);
+}
+
+async function getSwedishHolidays() {
+  const respons = await fetch("https://sholiday.faboul.se/dagar/v2.1/2024");
+  const data = await respons.json();
+  return data.dagar.filter((d) => d.helgdag);
 }
