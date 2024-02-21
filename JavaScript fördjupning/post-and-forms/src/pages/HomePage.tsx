b79100700cbac6ec1b109adeeca1becd5a3d2a9f
Michaela <mi-and@outlook.com>
2024-02-21 11:13:47 +0100

    Styled homepage

--- JavaScript fördjupning/post-and-forms/src/pages/HomePage.tsx
+++ JavaScript fördjupning/post-and-forms/src/pages/HomePage.tsx
@@ -2,10 +2,10 @@ import { mockedPosts } from "../posts";
 
 function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       {mockedPosts.map((post) => (
         <div key={post.id}>
-          <img src={post.url} className="w-full " />
+          <img src={post.url} className="w-full pb-2 h-96 object-cover" />
           <h2 className="text-xl font-bold px-2">{post.title}</h2>
           <p className="px-2 text-lg">{post.content}</p>
         </div>
