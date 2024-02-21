00573a54368ae80d7aceec28e367f93b2fc77e9f
Michaela <mi-and@outlook.com>
2024-02-21 11:22:08 +0100

    Add style in newportpage

--- JavaScript fördjupning/post-and-forms/src/pages/NewPostPage.tsx
+++ JavaScript fördjupning/post-and-forms/src/pages/NewPostPage.tsx
@@ -1,7 +1,25 @@
 function NewPostPage() {
   return (
-    <div>
-      <h2>NewPostPage</h2>
+    <div className="p-2 flex flex-col gap-4">
+      <h2 className="text-3xl">Skapa ett nytt inlägg</h2>
+
+      <input
+        className="border-green-200 border px-2 py-1"
+        placeholder="Bild url"
+        type="text"
+      />
+      <input
+        className="border-green-200 border px-2 py-1"
+        placeholder="Title"
+        type="text"
+      />
+
+      <textarea
+        className="border-green-200 border px-2 py-2"
+        placeholder="Text..."
+        rows={6}
+      ></textarea>
+      <button className="text-xl bg-green-200 p-2">Spara</button>
     </div>
   );
 }
