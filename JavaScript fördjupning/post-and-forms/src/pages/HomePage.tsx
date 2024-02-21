701a258cc1b2350d4437f7ed0758e96948edbe0d
Michaela <mi-and@outlook.com>
2024-02-21 10:53:36 +0100

    Add images

--- JavaScript fördjupning/post-and-forms/src/pages/HomePage.tsx
+++ JavaScript fördjupning/post-and-forms/src/pages/HomePage.tsx
@@ -1,7 +1,15 @@
+import { mockedPosts } from "../posts";
+
 function HomePage() {
   return (
     <div>
-      <h2>HomePage</h2>
+      {mockedPosts.map((post) => (
+        <div key={post.id}>
+          <img src={post.url} className="w-full " />
+          <h2 className="text-xl font-bold px-2">{post.title}</h2>
+          <p className="px-2 text-lg">{post.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
