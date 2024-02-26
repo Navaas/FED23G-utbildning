c5b831e7d95a8cea55e599c99540450b0293d7ec
Michaela <mi-and@outlook.com>
2024-02-26 09:53:34 +0100

    Add homepage,, favoritepage and routing

--- JavaScript fördjupning/react-context/src/main.tsx
+++ JavaScript fördjupning/react-context/src/main.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import AppLayout from "./AppLayout.tsx";
+import FavoritesPage from "./pages/FavoritesPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" Component={AppLayout}>
+      <Route index Component={HomePage} />
+      <Route path="favoriter" Component={FavoritesPage} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ JavaScript fördjupning/react-context/src/pages/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+function FavoritesPage() {
+  return (
+    <div>
+      <h2>FavoritePage</h2>
+    </div>
+  );
+}
+
+export default FavoritesPage;
