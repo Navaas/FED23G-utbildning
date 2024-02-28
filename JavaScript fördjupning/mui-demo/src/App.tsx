7ee90f858e71259b32ed7da7dfb25cce892beabe
Michaela <mi-and@outlook.com>
2024-02-28 10:26:10 +0100

    Start mui, create a button

--- JavaScript fördjupning/mui-demo/src/App.tsx
+++ JavaScript fördjupning/mui-demo/src/App.tsx
@@ -1,7 +1,10 @@
+import Button from "@mui/material/Button";
+
 function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <Button variant="contained">Hello world</Button>
     </>
   );
 }
