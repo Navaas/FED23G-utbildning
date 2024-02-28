b586bd1557c93657eccde3eb9f034230ee08ec01
Michaela <mi-and@outlook.com>
2024-02-28 10:46:45 +0100

    Add a component DrawerAppBar

--- JavaScript fördjupning/mui-demo/src/App.tsx
+++ JavaScript fördjupning/mui-demo/src/App.tsx
@@ -1,12 +1,7 @@
-import Button from "@mui/material/Button";
+import DrawerAppBar from "./DrawerAppBar";
 
 function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-      <Button variant="contained">Hello world</Button>
-    </>
-  );
+  return <DrawerAppBar />;
 }
 
 export default App;
