47b54b098820182630d29a6ee8857e73b4b7f7bf
Michaela <mi-and@outlook.com>
2024-03-04 11:28:14 +0100

    Add content to pages

--- JavaScript fördjupning/nextjs-demo/app/page.tsx
+++ JavaScript fördjupning/nextjs-demo/app/page.tsx
@@ -1,7 +1,51 @@
 export default function Home() {
   return (
-    <main>
-      <h2>HOME PAGE</h2>
+    <main className="p-4">
+      <p className="uppercase mt-4 mb-2">Documentation</p>
+      <hr className="border-stone-300" />
+      <h2 className="text-6xl mt-4 mb-8">Learn NextJS</h2>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
     </main>
   );
 }
--- JavaScript fördjupning/nextjs-demo/app/pricing/page.tsx
+++ JavaScript fördjupning/nextjs-demo/app/pricing/page.tsx
@@ -1,7 +1,51 @@
 export default function PricingPage() {
   return (
-    <main>
-      <h2>PRICING PAGE</h2>
+    <main className="p-4">
+      <p className="uppercase mt-4 mb-2">How must will it cost?</p>
+      <hr className="border-stone-300" />
+      <h2 className="text-6xl mt-4 mb-8">Pricing</h2>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
+      <p className=" py-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+        debitis suscipit sapiente voluptates obcaecati neque accusamus veniam
+        corporis vel quo ipsa quibusdam voluptas commodi eligendi vitae dicta
+        ullam, beatae nobis?
+      </p>
     </main>
   );
 }
