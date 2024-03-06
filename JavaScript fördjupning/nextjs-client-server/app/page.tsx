c726d5761a7f4dc6b7fd14ed63f23ae78a4f107b
Michaela <mi-and@outlook.com>
2024-03-06 11:23:05 +0100

    set up project with server components

---
+++ JavaScript fördjupning/nextjs-client-server/app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main className="p-4 flex flex-col gap-4">
+      <h1 className="text-xl my-4">Home</h1>
+      <p className="font-bold">Under rubrik</p>
+      <button className="py-2 px-4 bg-slate-200 font-semibold">
+        Click Count: 0
+      </button>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsum
+        esse amet tempora eius reiciendis labore non dolorem laborum quidem
+        totam facere possimus quos soluta, voluptatem odio assumenda, suscipit
+        consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ipsum
+        esse amet tempora eius reiciendis labore non dolorem laborum quidem
+        totam facere possimus quos soluta, voluptatem odio assumenda, suscipit
+        consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      </p>
+    </main>
+  );
+}
