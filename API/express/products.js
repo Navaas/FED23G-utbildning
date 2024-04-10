31d67236b493407876f61cd798e7276d9cef5204
Michaela <mi-and@outlook.com>
2024-04-10 10:53:47 +0200

    Mocked data

---
+++ API/express/products.js
@@ -0,0 +1,15 @@
+// Våra produkter
+const products = [
+  {
+    id: "1",
+    title: "Banankaka",
+    price: 85,
+  },
+  {
+    id: "2",
+    title: "Prinsesstårta",
+    price: 499,
+  },
+];
+
+module.exports = { products };
