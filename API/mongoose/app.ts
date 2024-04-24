aaa4eea9c120ec042c64802a7513e21894e486ee
Michaela <mi-and@outlook.com>
2024-04-24 14:17:48 +0200

    book handlers, routers schema och model

--- API/mongoose/app.ts
+++ API/mongoose/app.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+import { booksRouter } from "./resources/books/books-router";
+
+const app = express();
+
+/* Skapa ett unikt objectid */
+// const result = new Types.ObjectId();
+// console.log(result);
+
+app.use(express.json());
+app.use("/api/books", booksRouter);
+// app.use(authorRouter);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  res.status(500).json("Ett oväntat fel har uppstått");
+});
+/* Ska ligga i server.ts i inlämnings-kodbasen */
+main().catch((error) => console.error(error));
+
+/* ANSLUT TILL DATABASEN OCH STARTA SERVERN */
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI!);
+  app.listen(4400, () => console.log("http://localhost:4400"));
+}
