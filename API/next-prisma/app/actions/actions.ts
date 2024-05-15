98f20b203ca181a8ad2e6e938168d323e2ff17c1
Michaela <mi-and@outlook.com>
2024-05-15 11:21:09 +0200

    forkmnow client a component

--- API/next-prisma/app/actions/actions.ts
+++ API/next-prisma/app/actions/actions.ts
@@ -2,16 +2,13 @@
 
 import { db } from "@/prisma/db";
 import { revalidatePath } from "next/cache";
+import { PostCreate } from "../validation/post";
 
-export async function savePost(formData: FormData) {
-  console.log("savePost", formData.get("title"));
-  const title = formData.get("title")?.toString() || "Default title";
-  const content = formData.get("content")?.toString();
-
+export async function savePost(postData: PostCreate) {
   const post = await db.post.create({
     data: {
-      title,
-      content,
+      title: postData.title,
+      content: postData.content,
       authorId: 1,
     },
   });
