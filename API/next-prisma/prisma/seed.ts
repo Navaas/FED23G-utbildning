8b318e9272adefe66a94d9ebf10027568edf1858
Michaela <mi-and@outlook.com>
2024-05-15 09:43:54 +0200

    Setup prisma with seed

---
+++ API/next-prisma/prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function main() {
+  const mysan = await db.user.upsert({
+    where: { email: "mysan@mail.se" },
+    update: {},
+    create: {
+      email: "mysan@mail.se",
+      name: "Mysan",
+      posts: {
+        create: [
+          {
+            title: "Mjau",
+            content: "This is my first post",
+            published: true,
+          },
+          {
+            title: "Mjuaaaaau",
+            content: "This is my second post",
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+
+  console.log({ mysan });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
