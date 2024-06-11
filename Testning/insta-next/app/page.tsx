bf64811f299a1a172f41eb7b7b17ccc1501a1404
Michaela <mi-and@outlook.com>
2024-06-11 12:10:35 +0200

    Map out post i+on start page

--- Testning/insta-next/app/page.tsx
+++ Testning/insta-next/app/page.tsx
@@ -1,6 +1,10 @@
+import { db } from "@/prisma/db";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await db.post.findMany({
+    orderBy: { id: "desc" } /* Byter ordningen på listan */,
+  });
   return (
     <main>
       <Link href="/PostForm">
@@ -11,6 +15,13 @@ export default function Home() {
       </Link>
 
       <h1>Insta-next</h1>
+      {posts.map((post) => (
+        <div className="flex flex-col gap-2" key={post.id}>
+          <h2 className="text-xl">{post.title}</h2>
+          <p>{post.content}</p>
+          <img src={post.image} alt="bild" />
+        </div>
+      ))}
     </main>
   );
 }
