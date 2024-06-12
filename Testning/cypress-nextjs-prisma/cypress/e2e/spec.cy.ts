33cc42af295fa216f2e03c6717edc1da8f733f5d
Michaela <mi-and@outlook.com>
2024-06-12 13:30:55 +0200

    Add test

--- Testning/cypress-nextjs-prisma/cypress/e2e/spec.cy.ts
+++ Testning/cypress-nextjs-prisma/cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("/");
+    // cy.get("button").contains("Spara").click(); // Leta efter en knapp som innehåller texten "Spara" och klicka på den
+    cy.get("h1")
+      .contains("Welcome to my Blog")
+      .should("be.visible")
+      .should("exist"); // Leta efter en h1 som innehåller texten "Welcome to my Blog" och kolla att den är synlig och existerar
   });
 });
