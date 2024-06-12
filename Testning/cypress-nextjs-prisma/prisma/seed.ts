4761c4647a975c51b4917cec58224e287b546675
Michaela <mi-and@outlook.com>
2024-06-12 09:45:48 +0200

    Install prisma, set up db and seed one post

---
+++ Testning/cypress-nextjs-prisma/prisma/seed.ts
@@ -0,0 +1,24 @@
+import { db } from "./db";
+
+async function main() {
+  // Skapa en ny post
+  await db.post.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      title: "Hello World",
+      content: "This is a test post",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
