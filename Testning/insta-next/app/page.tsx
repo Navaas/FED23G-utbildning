cf17065ae8e62f4b1fbcbd702ae856d293df4f07
Michaela <mi-and@outlook.com>
2024-06-12 19:08:26 +0200

    center postcard on page

--- Testning/insta-next/app/page.tsx
+++ Testning/insta-next/app/page.tsx
@@ -2,7 +2,7 @@ import PostCard from "./ui/PostCard";
 
 export default async function Home() {
   return (
-    <main>
+    <main className="w-full md:w-4/5 flex flex-col items-center gap-4 p-4 mx-auto">
       <PostCard />
     </main>
   );
--- Testning/insta-next/app/ui/PostCard.tsx
+++ Testning/insta-next/app/ui/PostCard.tsx
@@ -6,21 +6,20 @@ async function PostCard() {
     orderBy: { id: "desc" } /* Byter ordningen på listan */,
   });
   return (
-    <div className="flex justify-center gap-8 flex-col items-center md:flex-row">
+    <div className="flex gap-8 flex-col  md:flex-row">
       {posts.map((post) => (
-        <div key={post.id}>
-          <h2 className="text-xl">{post.title}</h2>
-          <p>{post.content}</p>
-          <div className="w-350 h-350 relative">
+        <div key={post.id} className="bg-black text-white p-4 w-full h-96 ">
+          <div className="relative w-full  h-2/3">
             <Image
               src={post.image}
               alt="Description of image"
-              width={350}
-              height={350}
+              layout="fill" // Gör att bilden fyller hela föräldrarens storlek
               objectFit="cover"
-              className="rounded-md"
+              className="object-cover"
             />
           </div>
+          <h2 className="text-xl">{post.title}</h2>
+          <p>{post.content}</p>
         </div>
       ))}
     </div>
