0ca84df00e9a2547aa6503bca2c71c5b7a7252a2
Michaela <mi-and@outlook.com>
2024-06-12 13:53:33 +0200

    reset and seed

--- Testning/cypress-nextjs-prisma/cypress/e2e/spec.cy.ts
+++ Testning/cypress-nextjs-prisma/cypress/e2e/spec.cy.ts
@@ -1,10 +1,14 @@
 describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("/");
-    // cy.get("button").contains("Spara").click(); // Leta efter en knapp som innehåller texten "Spara" och klicka på den
-    cy.get("h1")
-      .contains("Welcome to my Blog")
-      .should("be.visible")
-      .should("exist"); // Leta efter en h1 som innehåller texten "Welcome to my Blog" och kolla att den är synlig och existerar
+  beforeEach(() => {
+    cy.exec("npm run reset && npm run seed");
   });
+});
+
+it("passes", () => {
+  cy.visit("/");
+  // cy.get("button").contains("Spara").click(); // Leta efter en knapp som innehåller texten "Spara" och klicka på den
+  cy.get("h1")
+    .contains("Welcome to my Blog")
+    .should("be.visible")
+    .should("exist"); // Leta efter en h1 som innehåller texten "Welcome to my Blog" och kolla att den är synlig och existerar
 });
