b862586759725223d7e47e0d98e94ac75bc5db24
Michaela <mi-and@outlook.com>
2024-06-12 19:09:29 +0200

    link to startpage in header

--- Testning/insta-next/app/ui/Header.tsx
+++ Testning/insta-next/app/ui/Header.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="bg-slate-400 p-4 flex justify-between">
-      <h1 className="text-black">Insta-next</h1>
+      <Link href="/">
+        <h1 className="text-black">Insta-next</h1>
+      </Link>
       <Link href="/NewPostPage">
         <span className="text-2xl">➕</span>
       </Link>
