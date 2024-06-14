3c39b65703bf5a92fc2ba230cea51bdece8b9564
Michaela <mi-and@outlook.com>
2024-06-14 10:12:27 +0200

    add new icon and changed color

--- Testning/insta-next/app/ui/Header.tsx
+++ Testning/insta-next/app/ui/Header.tsx
@@ -3,12 +3,25 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <div className="bg-slate-400 p-4 flex justify-between">
+      <div className="bg-blue-300 p-4 flex justify-between">
         <Link href="/">
-          <h1 className="text-black">Insta-next</h1>
+          <h1 className="text-blue-950">Insta-next</h1>
         </Link>
         <Link href="/NewPostPage">
-          <span className="text-2xl">➕</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-10 cursor-pointer text-blue-950"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
         </Link>
       </div>
     </header>
