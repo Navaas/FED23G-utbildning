1923aed42d35503c779bb2763c40094b5a57b5fe
Michaela <mi-and@outlook.com>
2024-08-12 10:18:43 +0200

    Create countbutton component

--- Testning/demo-enhets-test/src/sum.test.ts
+++ Testning/demo-enhets-test/src/sum.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, it } from "vitest";
 import { sum } from "./sum";
 
+// 1. Hämta funktionen sum från sum.ts
+// 2. Förklara vad funktionen gör
+// 3. Expect vad funktionen returnerar när den anropas med 1 och 2
 describe("Sum", () => {
   it("should sum two values", () => {
     expect(sum(1, 2)).toBe(3);
